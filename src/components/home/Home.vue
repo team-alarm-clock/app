c2f882b7f95caf84d056a456b84f0d21cb6c17c2
Abel Quintero <abel.j.quintero@gmail.com>
2018-12-17 14:53:55 -0800

    data for entire artist object showing

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,18 +1,21 @@
 <template>
     <section>
         <h2>THIS IS THE HOME PAGE</h2>
+        {{result}}
     </section>
 </template>
 
 <script>
 import api from '../../services/api.js';
 export default {
+  data() {
+    return {
+      result: null
+    };
+  },
   created() {
     api.getArtists()
-      .then(response => {
-        console.log('got here');
-        this.artist = response.artist;
-      });
+      .then(result => this.result = result);
   }
 
 };

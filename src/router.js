a05e389e13a03a2a986abe6154f33ae3f8d73c5b
Carmen V Ramos <carmenvramos@yahoo.com>
2018-12-17 14:53:31 -0800

    Added components About,Artists,Profiles and added routes to router.js

--- src/router.js
+++ src/router.js
@@ -1,12 +1,17 @@
 import VueRouter from 'vue-router';
-import Home from './components/home/Home.vue';
+import Home from './components/home/Home';
+import Artists from './components/artists/Artists';
+import Profiles from './components/profiles/Profiles';
+import About from './components/about/About';
 
 
 export default new VueRouter({
 
   routes: [
     { path: '/', component: Home },
-    
+    { path: '/artists', component: Artists },
+    { path: '/profiles', component: Profiles },
+    { path: '/about', component: About },
     { path: '*', redirect: '/' }
   ]
 });

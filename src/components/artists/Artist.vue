375c38567f01a6e041e2a4f9c0d7bf9d62995ce9
Paige Gorry <paigeegorry@gmail.com>
2018-12-18 15:56:19 -0800

    ready to branch and pair program

---
+++ src/components/artists/Artist.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+};
+</script>
+
+<style>
+img {
+  height: 100px;
+}
+li {
+  list-style: none;
+}
+</style>

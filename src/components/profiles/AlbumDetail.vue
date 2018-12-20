31d1bb5caefb9f6add9c31bf04499e585bdff365
Abel Quintero <abel.j.quintero@gmail.com>
2018-12-19 16:35:26 -0800

    fixing merge conflict

---
+++ src/components/profiles/AlbumDetail.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="badges-container">
+    <h2>My Badges</h2>
+    <div class="badges">
+      <font-awesome-icon icon="fas fa-headphones-alt" />
+      <font-awesome-icon icon="far fa-calendar-alt" />
+      <font-awesome-icon icon="fas fa-trophy" />
+
+    </div>
+
+    <section>
+      <h2>My favorite artists</h2>
+    </section>
+
+    <RouterLink to="/search">Search</RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>

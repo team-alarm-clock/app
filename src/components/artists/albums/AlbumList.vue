28764cda20aa2f5b4a9294652d21121778ad0605
‘Michael <pickup.patterns@gmail.com>
2018-12-19 16:23:43 -0800

    decpoupled album logic and put it on other component styled a grid for css

--- src/components/artists/albums/AlbumList.vue
+++ src/components/artists/albums/AlbumList.vue
@@ -1,9 +1,9 @@
 <template>
-  <ul>
-    <Album v-for="release in releases"
-            :key="release.id" 
-            :releases="releases"/>
-  </ul>
+
+     <ul class="albums">
+      <Album v-for="release in releases" :key="release.id" :release="release"/>
+    </ul>
+
 </template>
 
 <script>
@@ -19,5 +19,21 @@ export default {
 </script>
 
 <style>
-
+.albums {
+    padding: 0;
+    margin-top:.5%;    
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(375px, 1fr));
+    grid-gap: 10px;
+    grid-auto-rows: 500px;
+}
+.albums li {
+    list-style: none;
+    text-align: center;
+    text-transform: uppercase;
+    color: #000000;
+    padding: 15px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+}
 </style>

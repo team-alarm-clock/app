7fb502e5ce9c08758e6759595062de8744cab282
Abel Quintero <abel.j.quintero@gmail.com>
2018-12-20 14:39:50 -0800

    getting rid of fontawesome

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import VueRouter from 'vue-router';
 import App from './components/App.vue';
 import router from './router';
 import './main.css';
-import fontawesome from '@fortawesome/fontawesome';
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
-import falight from '@fortawesome/fontawesome-pro-light';
-import fasolid from '@fortawesome/fontawesome-pro-solid';
+// import fontawesome from '@fortawesome/fontawesome';
+// import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+// import falight from '@fortawesome/fontawesome-pro-light';
+// import fasolid from '@fortawesome/fontawesome-pro-solid';
 
-fontawesome.library.add(falight, fasolid);
+// fontawesome.library.add(falight, fasolid);
 
-Vue.component(FontAwesomeIcon.name, FontAwesomeIcon);
+// Vue.component(FontAwesomeIcon.name, FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);

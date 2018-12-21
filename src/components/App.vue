dd9f42811c00566423a1ed46703a7026d3ee54ee
‘Michael <pickup.patterns@gmail.com>
2018-12-21 04:59:12 -0800

    cleans up App.vue

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-  <!-- <font-awesome-icon icon="user-secret" /> -->
    <header>
      <nav v-if="user">
         <Header />
@@ -94,5 +93,4 @@ export default {
   top: 0;
   right: 0;
 }
-
 </style>
